471a99ba86ffdb5881dd8c591d7056d9433ad7da
sabur786 <imtiazrizvi786@yahoo.com>
2025-07-02 08:54:42 -0500

    added register and login routing

--- todo-ui/src/App.jsx
+++ todo-ui/src/App.jsx
@@ -6,6 +6,8 @@ import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import TodoComponent from './components/TodoComponent'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RegisterComponent from './components/RegisterComponent'
+import LoginComponent from './components/LoginComponent'
 
 function App() {
 
@@ -22,8 +24,12 @@ function App() {
           <Route path='/todos' element = { <ListTodoComponent  /> }></Route>
             {/* // http://localhost:8080/add-todo */}
           <Route path='/add-todo' element = { <TodoComponent  /> }></Route> 
-                      {/* // http://localhost:8080/update-todo/1 */}
-          <Route path='/update-todo/:id' element = { <TodoComponent  /> }></Route>   
+            {/* // http://localhost:8080/update-todo/1 */}
+          <Route path='/update-todo/:id' element = { <TodoComponent  /> }></Route> 
+            {/* // http://localhost:8080/register */}
+          <Route path='/register' element = { <RegisterComponent/> }></Route>
+            {/* // http://localhost:8080/login */}
+          <Route path='/login' element = { <LoginComponent  /> }></Route>
 
       </Routes>
      <FooterComponent />
--- todo-ui/src/components/HeaderComponent.jsx
+++ todo-ui/src/components/HeaderComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const HeaderComponent = () => {
   return (
@@ -9,9 +10,27 @@ const HeaderComponent = () => {
                             <a href='http://localhost:3000' className='navbar-brand'>
                                     Todo Management Aplication    
                             </a>
-
                   </div>
-
+                  <div className='collapse navbar-collapse'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                              <NavLink to="/todos" clasName="nav-link" >Todo</NavLink>
+                        </li>
+                        </ul>
+                        </div>
+                        <div className='collapse navbar-collapse'>
+                            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                              <li className='nav-item'>
+                                  <NavLink to="/login" clasName="nav-link" >Login</NavLink>
+                              </li>
+                            </ul>
+                        </div> 
+                        <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                              <NavLink to="/register" clasName="nav-link" >Register</NavLink>
+                        </li>
+                        </ul>
+                        
                </nav>
         </header>
     </div>
